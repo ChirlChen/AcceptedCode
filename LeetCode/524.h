7732a63042748ba1cf1b2e0e76521c284b15a1ec
Chirl <ChirlChen@163.com>
2017-04-05 20:12:32 +0800

    Add a interview programming test solution

--- LeetCode/524.h
+++ LeetCode/524.h
@@ -27,11 +27,11 @@ namespace Chirl
     public:
         /************************************************************************
          思路： 依次从d中取出一个字符串bs;
-                逐字符去查看bs中的字符在s中是否存在;
-                对于同一个字符串bs，s中找过的字符不再重复查找;
+                逐字符去查看ds中的字符在s中是否存在;
+                对于同一个字符串ds，s中找过的字符不再重复查找;
                 如果, bs中所有的字符在s中都能找到，则该字符是可能的返回值;
                       反之则不是可能的返回值;
-                返回最终找到的最长且在b中下标最小的字符串bs;
+                返回最终找到的最长且字符序最小的字符串;
          算法复杂度：;
                 s的长度：m; d的长度：n; d中字符串平均长度：k
                 复杂度：O(m*n);
@@ -44,11 +44,10 @@ namespace Chirl
             }
 
             string resultStr;           //可能的结果字符串;
-            int curMaxLen = 0;          //当前字符型的最大长度; 
             int dictSize = d.size();
             for (int i = 0; i < dictSize; i++)
             {
-                if (d[i].size() > s.size() || d[i].size() < curMaxLen)
+                if (d[i].size() > s.size() || d[i].size() < resultStr.size())
                 {
                     continue;
                 }
@@ -63,13 +62,8 @@ namespace Chirl
                     }
                     if ('\0' == *pds)   //字典d中当前字符被全部找到;
                     {
-                        if (curMaxLen < d[i].size()) //如果为目前最长，则更新返回结果;
-                        {
-                            resultStr = d[i];
-                            curMaxLen = d[i].size();
-                        }
-                        else if (curMaxLen == d[i].size() && 
-                                 0 > strcmp(d[i].c_str(), resultStr.c_str()))
+                        if (resultStr.size() < d[i].size() ||               //如果比上次结果更长，则更新返回结果;
+                            0 > strcmp(d[i].c_str(), resultStr.c_str()))    //等长但是字典序更小;
                         {
                             resultStr = d[i];
                         }
@@ -81,10 +75,25 @@ namespace Chirl
             return resultStr;
         }
   
+        string findLongestWord_1(string s, vector<string>& d)   //网上其他人的方案（https://discuss.leetcode.com/topic/80876/10-lines-solutions-for-c），思路一致,但代码很精练;
+        {
+            string ans;                                         
+            for (int i = 0; i < d.size(); i++) 
+            {
+                int pi = 0, pj = 0;
+                for (; pi < s.size() && pj < d[i].size(); pi++) 
+                {
+                    pj += s[pi] == d[i][pj];                    //这一行很精妙;
+                }
+                if (pj == d[i].size() && (ans.size() < d[i].size() || (ans.size() == d[i].size() && strcmp(ans.c_str(),d[i].c_str()))))
+                    ans = d[i];
+            }
+            return ans;
+        }
         static void test()
         {
-            string s = "bab";//"abpcplea";
-            string tmps[] = {"ba","ab","a","b"};//{"ale","apple","monkey","plea"};
+            string s = "abpcplea";//"bab";//
+            string tmps[] = {"ale","apple","monkey","plea"};//{"ba","ab","a","b"};//
             vector<string> d(begin(tmps), end(tmps)); 
             
             Solution_524 su;
