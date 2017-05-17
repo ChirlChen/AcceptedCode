c53a3e4a8cc937cb61f9ab651c9c7c33e9a6642c
CheilQuan <ChirlChen@163.com>
2017-05-17 21:45:56 +0800

    sync

--- LeetCode/main.cpp
+++ LeetCode/main.cpp
@@ -1,10 +1,13 @@
 ﻿#include "524.h"
 #include "301.h"
+#include <list>
+#include <typeinfo>
+#include <iostream>
 
 //namespace list: Chirl | NanerLee | Sansan | Wcytem...
 int main(int argc, char **argv)
 {
+    std::cout << typeid(std::remove_const<const int[10]>::type).name(); 
     Chirl::Solution_301::test();  
-  
     return 0;
 }
