6f9539327ca166892b4186161a6724f7471a6f82
ChirlChen <ChirlChen@163.com>
2017-07-28 17:32:11 +0800

    add 2 solutions

---
+++ NowCoder/CodingInterviews/31.h
@@ -0,0 +1,84 @@
+﻿/************************************************
+  FileName    : 31.h           ;
+  Description :                                 ;
+  Creator     : Chirl                           ;
+  Contact     : ChirlChen@163.com               ;
+  CreatedDate : 2017/07/28                      ;
+              _                                 ;
+             /  \         吉                    ;     
+            /|oo \         祥                   ;    
+           (_|  /_)       物                    ;   
+             `@/  \   _    镇                   ;    
+             |     \  \\  楼                    ;   
+              \||   \  ))  !!                   ;    
+              |||\ / \//                        ; 
+            _//|| _\  /                         ;
+    ______(_/(_|(____/_________                 ;
+*************************************************/
+#ifndef _31_H_
+#define _31_H_
+#include <string>
+#include <vector>
+#include <set>
+using namespace std;
+
+namespace Chirl
+{
+    /************************************************************************
+     思      路： 
+     算法复杂度：
+     ************************************************************************/
+    class Solution_31
+    {
+    private:
+        struct compare {
+            bool operator()(const string &lh, const string &rh) const{
+                int i = 0;
+                for(; i < lh.length() && i < rh.length(); ++i){
+                    if(lh[i] != rh[i]) return lh[i] < rh[i];
+                }
+
+                if(lh.length() == rh.length()) return false;
+
+                if(i == lh.length()){
+                    return operator()(lh, string(&rh[i])) ? 1 : lh.length() < rh.length();
+                }
+                else{
+                    return operator()(string(&lh[i]), rh) ? 1 : lh.length() < rh.length();
+                }
+            }
+        } ;
+    public:
+        string PrintMinNumber(vector<int> numbers) {
+            if(numbers.empty()) return string("");
+
+            set<string, compare> nums;
+            //nums.reserve(numbers.size());
+
+            for(int i = 0; i < numbers.size(); ++i){
+                nums.insert(to_string((long long)numbers[i]));
+            }
+
+            string ans;
+            for(auto itr = nums.begin(); itr != nums.end(); ++itr){
+                ans.append(*itr);
+            }
+
+            return ans;
+        }
+
+
+        static void test()
+        {
+            Solution_31 su;
+            int tmp[] = {1,11,1111};
+            vector<int> nums(begin(tmp), end(tmp));
+            su.PrintMinNumber(nums);
+ 
+        }
+    };
+  
+}
+
+
+#endif //!_31_H_
---
+++ NowCoder/CodingInterviews/32.h
@@ -0,0 +1,73 @@
+﻿/************************************************
+  FileName    : 32.h           ;
+  Description :                                 ;
+  Creator     : Chirl                           ;
+  Contact     : ChirlChen@163.com               ;
+  CreatedDate : 2017/07/28                      ;
+              _                                 ;
+             /  \         吉                    ;     
+            /|oo \         祥                   ;    
+           (_|  /_)       物                    ;   
+             `@/  \   _    镇                   ;    
+             |     \  \\  楼                    ;   
+              \||   \  ))  !!                   ;    
+              |||\ / \//                        ; 
+            _//|| _\  /                         ;
+    ______(_/(_|(____/_________                 ;
+*************************************************/
+#ifndef _32_H_
+#define _32_H_
+#include <vector>
+
+using namespace std;
+
+namespace Chirl
+{
+    /************************************************************************
+     思      路： 
+     算法复杂度：
+     ************************************************************************/
+    class Solution_32
+    {
+    public:
+        int GetUglyNumber_Solution(int index) {
+            if(index <= 0) return 0;
+
+            int tmp[] = {1, 2, 3, 4, 5};
+            vector<int> uglyNum(begin(tmp), end(tmp));
+            uglyNum.reserve(index);
+
+            if(index <= 5) return tmp[index - 1];
+
+            int idx2 = 2, idx3 = 2, idx5 = 2;
+            int ugly2 = 0, ugly3 = 0, ugly5 = 0;
+            while(uglyNum.size() < index) {
+                int curMax = *uglyNum.rbegin();
+
+                while(ugly2 <= curMax){
+                    ugly2 = 2 * uglyNum[idx2++];
+                }
+                while(ugly3 <= curMax){
+                    ugly3 = 3 * uglyNum[idx3++];
+                }
+                while(ugly5 <= curMax){
+                    ugly5 = 5 * uglyNum[idx5++];
+                }
+
+                uglyNum.push_back(min(min(ugly2, ugly3), ugly5));
+            }
+
+            return *uglyNum.rbegin();
+        }
+  
+        static void test()
+        {
+            Solution_32 su;
+            int ans = su.GetUglyNumber_Solution(6);
+        }
+    };
+  
+}
+
+
+#endif //!_32_H_
